a64cf012f1ef6a2245e93ad860d96167872e16fc
amitthadani36 <amitthadani36@gmail.com>
2025-07-09 22:56:43 +0530

    car purchase code

---
+++ car_dekho.py
@@ -0,0 +1,15 @@
+product_mileage = 19.1
+product_fuel_type = "Diesel"
+product_engine_displacement = 1493
+product_no_of_cylinders = 4
+product_max_power = "114 bhp @ 4000 rpm"
+product_max_torque = "250 Nm @ 1500-2750 rpm"
+product_seating_capacity = 5
+print("Key Specifications of Hyundai Creta:")
+print("ARAI Mileage:", product_mileage)
+print("Fuel Type:", product_fuel_type)
+print("Engine Displacement:", product_engine_displacement)
+print("No. of Cylinders:", product_no_of_cylinders)
+print("Max Power:", product_max_power)
+print("Max Torque:", product_max_torque)
+print("Seating Capacity:", product_seating_capacity)
