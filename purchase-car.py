d4ed3987746d3074c64b7aefa85286763b2b3d2a
amitthadani36 <amitthadani36@gmail.com>
2025-07-12 08:41:59 +0530

    written code

--- purchase-car.py
+++ purchase-car.py
@@ -1,17 +1,19 @@
 car_variant = "z2E petrol"
-car_price = 2000000
+car_price = 2007695
 time_period = 5
 rate_of_interest = 8
 initial_down_payment = 500000
-simple_interest = (car_price - initial_down_payment) * rate_of_interest * time_period / 100
-total_amount = car_price - initial_down_payment + simple_interest
-monthly_installment = total_amount / (time_period * 12)
+loan_amount = car_price - initial_down_payment
+months = time_period * 12
+monthly_interest_rate = rate_of_interest / (12*100)
+emi = (loan_amount * monthly_interest_rate * ((1+monthly_interest_rate) ** months)) / (((1+monthly_interest_rate) ** months) - 1)
+total_amount = emi* time_period *12
 print("======================")
 print(f"Car Variant: {car_variant}")
 print(f"Car Price: {car_price}")
 print(f"Time Period: {time_period} years")
 print(f"Rate of Interest: {rate_of_interest}%")
 print(f"Initial Down Payment: {initial_down_payment}")
-print(f"Simple Interest: {simple_interest}")
+print(f"loan amount: {loan_amount}")
 print(f"Total Amount Payable: {total_amount}")
-print(f"Monthly Installment: {monthly_installment}")
+print(f"Monthly Installment: {emi}")
